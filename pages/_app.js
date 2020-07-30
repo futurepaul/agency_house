ae1289158df086c4cae423bc735349031b28700a
Paul Miller <paul@pauljmiller.com>
2020-07-30 09:08:13 -0400

    get rid of fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,4 @@
 import "../styles/global.css";
-require("typeface-ibm-plex-sans");
-require("typeface-lexend-zetta");
-require("typeface-lexend-exa");
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
